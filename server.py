29af2244d4d8c9233b8de31031610e31def38222
Brian Whiteley <brianwhiteleybw@gmail.com>
2021-01-03 22:20:35 -0600

    add blgo

--- server.py
+++ server.py
@@ -19,7 +19,14 @@ def guess(name):
     age_data = requests.get(f"https://api.agify.io?name={name}")
     age = age_data.json()['age']
     return render_template("guess.html", name = name, age=age, sex=sex)
-    
+
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
     
 
 
